814293352b68044948ee6ab21ae8fe36b83a9d5f
Salvo Falcone <salvofalcone57@gmail.com>
2023-08-04 03:29:42 +0200

    fix: changed date and time format, styled card highlight

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,84 @@
 import React from "react";
 
+import RightArrow from "@/components/Icons/RightArrow";
+
 import styles from "./Card.module.scss";
 
 const Card = ({ travelInfo }) => {
+  if (travelInfo && travelInfo.departureDate) {
+    const departureInfo = travelInfo?.departureDate?.split(" ");
+    const [depYear, depMonth, depDay] = departureInfo[0].split("-");
+    const depDate = depDay + "/" + depMonth + "/" + depYear;
+    const [depHour, depMin, depSec] = departureInfo[1].split(":");
+    const depTime = depHour + ":" + depMin;
+
+    const arrivalInfo = travelInfo?.arrivalDate?.split(" ");
+    const [arrYear, arrMonth, arrDay] = arrivalInfo[0].split("-");
+    const arrDate = arrDay + "/" + arrMonth + "/" + arrYear;
+    const [arrHour, arrMin, arrSec] = arrivalInfo[1].split(":");
+    const arrTime = arrHour + ":" + arrMin;
+
+    return (
+      <div className={styles.Card}>
+        <div className={styles.Card__Top}>
+          <p>
+            <span className={styles.Card__Top__Price}>
+              {travelInfo?.budget?.value}
+              {travelInfo?.budget?.currencyCode}
+            </span>{" "}
+            {travelInfo?.budget?.costType}
+          </p>
+          <h2 className={styles.Card__Top__Title}>{travelInfo?.title}</h2>
+        </div>
+
+        <div className={styles.Card__Content}>
+          <div className={styles.Card__Content__Travel}>
+            <div className={styles.Card__Content__Travel__Info}>
+              <div>
+                <p className={styles.Card__Content__Light}>PARTENZA DA</p>
+                <h3 className={styles.Card__Content__Bold}>
+                  {travelInfo?.departure?.Port}
+                </h3>
+              </div>
+              <div className={styles.Card__Content__Bold}>
+                <p>{travelInfo?.boatType}</p>
+                <p>{travelInfo?.numberOfDays} giorni</p>
+              </div>
+            </div>
+
+            <div className={styles.Card__Content__Travel__Info}>
+              <div>
+                <p>{depDate}</p>
+                <p className={styles.Card__Content__Light}>{depTime}</p>
+              </div>
+              <div>
+                <RightArrow />
+              </div>
+              <div>
+                <p>{arrDate}</p>
+                <p className={styles.Card__Content__Light}>{arrTime}</p>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.Card__Content__Availability}>
+            <p>
+              {travelInfo?.reservations} {travelInfo?.reservationsType}
+            </p>
+            <p>
+              <span className={styles.Card__Content__Light}>Disponibilità</span>{" "}
+              <span className={styles.Card__Content__Bold}>
+                {travelInfo?.reservationsAvailable}
+              </span>
+            </p>
+          </div>
+        </div>
+
+        <button className={styles.Card__Button}>Prenota</button>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.Card}>
       <div className={styles.Card__Top}>
---
+++ src/components/Icons/RightArrow.jsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+import styles from "./Icons.module.scss";
+
+const SvgComponent = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+    className={styles.RightArrow}>
+    <path
+      fill="#273049"
+      d="m11.293 17.293 1.414 1.414L19.414 12l-6.707-6.707-1.414 1.414L15.586 11H6v2h9.586z"
+    />
+  </svg>
+);
+export default SvgComponent;
